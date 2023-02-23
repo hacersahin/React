0f046b2297655be1d50a2a1bb95625ab58b64834
Hacer Şahin <120566057+hacersahin@users.noreply.github.com>
2023-02-23 14:44:50 +0300

    Update navlist.js

--- navlist.js
+++ navlist.js
@@ -1,9 +1,35 @@
 function NavList(props){
+
+
+    const sitil={
+        display:'inline-block',
+        margin:'0 10px',
+        
+    }
+    const linktus= {
+        textDecoration:'none',
+        backgroundColor:'transparent',
+        color:'black',
+        border: '1px solid',
+        borderRadius: '5px',
+        padding: '10px',
+        margin: '5',
+        display:'inline-block',
+        transition:'background-color 0.2 seconds'
+        
+        
+
+    };
+    const hoverstili={
+        backgroundColor:'pink',
+    };
     return(
 
-        <li>
-           <a href={"#"} onClick={props.tiklandiginda}> 
-           {props.menuElemani}
+        <li style={sitil}>
+           <a href={"#"} onClick={props.tiklandiginda} style={linktus} 
+           onMouseOver= {(e)=> Object.assign(e.target.style, hoverstili)}
+           onMouseOut= {(e)=> Object.assign(e.target.style, linktus)}> 
+           {props.menuElemani} 
            </a>
            
         </li>
