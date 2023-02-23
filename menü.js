5cec7cfcf20ba9dbb7bf5be1f129858eff2cb042
Hacer Şahin <120566057+hacersahin@users.noreply.github.com>
2023-02-23 14:44:08 +0300

    Update menü.js

--- menü.js
+++ menü.js
@@ -15,7 +15,7 @@ function Menu() {
     //TEKRAR KULLANILABİLEN DEĞERLER
 
     const baslik= ["TV","Sinema","Tiyatro","Kitap", "Müzik"];
-    const aciklama =["TV Tarihi","Sinema Tarihi","Tiyatronun Tarihi","Yazının Tarihi","Müzik Tarihi"];
+    const aciklama =["TV Tarihi","1891 yılında Amerika’da Thomas Edison ile yardımcısı William Kennedy Laurie Dickson’ın geliştirdiği “Kinetoscope” prototipi ilk film görüntüleme makinelerinden biridir. Cihazın üst kısmında bulunan bir delikten hareketli resimlerin izlenebildiği bu makine, aynı zamanda sinematik projeksiyon aletlerinin de temelini oluşturmaktadır.","Tiyatronun Tarihi","İlk yazı yüzeyi hem sert oluşu hem de yazılmak için bir hazırlığa ihtiyaç duymaması nedeniyle taş olarak bilinir. Prehistorik dönemde insanların fikirlerini somut ve görünür kılmak için kullandıkları taş yüzeyler kimi zaman mağara duvarlarındaki ritüel çizimleri olarak ortaya çıkarken kimi zaman da küçük taşlar üzerinde geometrik işaretler olarak gözlemlenir. Paleolitik çağda Kuzey İspanya’da,Fransa’da ve İngiltere’de bunun gibi küçük taşların üzerindeki yazılara veya başka bir ifadeyle sembollere rastlanır. Eski çağ sonlarına kadar taş yüzeyi üzerindeki yazıların aktif şekilde kullanıldığı bilinir","Müzik, geçmiş ve şimdiki bilinen her kültür ve dinde zaman ve mekânlar arasında büyük çeşitlilik gösterir. En izole edilmiş kabile grupları da dâhil olmak üzere dünyadaki tüm insanlar bir müzik türüne sahip olduklarından, müziğin, insanların dünyaya yayılmasından önce atalardan oluşan topluluklarda mevcut olduğu sonucuna varılabilir. Sonuç olarak, ilk müzik Afrika'da icat edilmiş ve daha sonra çeşitli çalgılar yapmak için çeşitli materyaller kullanarak insan hayatının temel bir bileşeni hâline gelmiş olabilir."];
     const resim= [tv,sinema,tiyatro,yazi,müzik];
 
     //RENDER FONKSİYONU
@@ -36,7 +36,14 @@ function Menu() {
 
     return(
       <>
-      <ul>
+      <div style={{border:'1px solid black'}}>
+      <ul style={{
+        listStyleType: "none",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+       
+      }}>
         {baslik.map((eleman,index) => 
 
          (<NavList
@@ -45,6 +52,7 @@ function Menu() {
         
         )}
       </ul>
+      </div>
       <div id="detay">
 
       </div>
