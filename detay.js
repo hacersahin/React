befb7b7c418e22ee7039762fef6fc4ca9a1fdaca
Hacer Şahin <120566057+hacersahin@users.noreply.github.com>
2023-02-23 14:41:53 +0300

    Update detay.js

--- detay.js
+++ detay.js
@@ -1,10 +1,37 @@
 function Detay(props){
+
+
+    const ozellik = {
+
+        width: "50%",
+        height: "50%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        margin: "auto",
+        padding: "50px",
+        border: "1px solid black",
+    };
+
+    const img = {
+        width: "50%",
+        height: "50%",
+        margin: "auto",
+        padding: "50px",
+        display: "block",
+    };
+   
+      
     
     return(
 
         <>
-        <img src={props.resim}/>
-        <p> {props.aciklama}</p>
+        <div style={ozellik}>
+
+                <img src={props.resim} style={{img}}/>
+                <p > {props.aciklama}</p>
+
+        </div>
         
         </>
        
